18e0fec4fed958058182e72ae6630a19233d443a
Shawn Taylor <shawncurtistaylor@gmail.com>
2019-12-05 00:52:34 -0800

    hello

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,15 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says hi honey', (done) => {
+  chai.request(server)
+    .get('/')
+    .end((err, res) => {
+      expect(err).not.exist;
+      expect(JSON.stringify(res.text)).to.contain('Hi Honey');
+      done();
+    });
+  });
+
 })
